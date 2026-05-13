ad15355de816faed5994bc66e19d66c9fa4a3638
Hetvi_Pandya <pandyahetvi121@gmail.com>
2026-05-13 16:28:05 +0530

    Initial commit

--- BASEURL.js
+++ BASEURL.js
@@ -1,3 +1,3 @@
-const BASE_URL = "https://harsh-patel-holly-zolly-backend.onrender.com";
+const BASE_URL = "https://final-hz-b.onrender.com"
 // const BASE_URL = "https://harsh-patel-holly-zolly-backend.onrender.com";
 export default BASE_URL;
